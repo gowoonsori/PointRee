9a5fab1a51896594fd336985d9d81dbd7b729908
gowoonsori <gowoonsori97@naver.com>
2021-05-02 23:57:09 +0900

    Add : cors 설정 (모두 허용)

--- src/main/java/gowoo/pointree/configs/SecurityConfig.java
+++ src/main/java/gowoo/pointree/configs/SecurityConfig.java
@@ -5,12 +5,16 @@ import gowoo.pointree.security.Jwt;
 import gowoo.pointree.security.JwtAccessDeniedHandler;
 import gowoo.pointree.security.JwtAuthenticationTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +31,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // Disable CSRF Token generation
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+                .csrf().disable()
+                // Disable the default HTTP Basic-Auth
+                .httpBasic().disable()
+                // Disable the /logout filter
+                .logout().disable()
+                .headers().disable()
                 .addFilterBefore(new JwtAuthenticationTokenFilter(jwtTokenConfig.getHeader(), jwt),
                         UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
@@ -37,17 +52,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler)
-                .authenticationEntryPoint(unauthorizedHandler)
-                .and()
-                // Disable the session management filter
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                // Disable CSRF Token generation
-                .csrf().disable()
-                // Disable the default HTTP Basic-Auth
-                .httpBasic().disable()
-                // Disable the /logout filter
-                .logout().disable()
-                .headers().disable();
+                .authenticationEntryPoint(unauthorizedHandler);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
